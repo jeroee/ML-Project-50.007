4d2022ebe1282b35980a1b0eb875bfe87712a618
jeroee <jeroejeroe@gmail.com>
2020-12-01 21:15:11 +0800

    Improve code time complexity for part 2

--- Part 2.py
+++ Part 2.py
@@ -10,18 +10,13 @@ SG_test = 'SG/dev.in'
 CN_test = 'CN/dev.in'
 
 # load training file
-
-
 def load_train(training_file):
     df = pd.read_csv(training_file, sep=' ',
                      header=None, error_bad_lines=False)
     df.columns = ['word', 'state']
     return df
 
-# def load_test(test_file):
-#     df = pd.read_csv(test_file, sep=' ', header = None, error_bad_lines=False)
-#     df.columns=['word']
-#     return df
+# load testing file
 def load_test(testing_file):
     ls = []
     f = open(testing_file)
@@ -32,90 +27,165 @@ def load_test(testing_file):
     return df
 
 # load empty emission probability table
-def create_matrix(df):
-    new_df = pd.DataFrame(columns=df['state'].unique().tolist())
-    unique_words = df['word'].unique().tolist()
-    new_df['unique_words'] = unique_words
-    # Commented out for now
-    # new_df = new_df.set_index('unique_words')
+def createMatrix(df):
+    start = time.time()
+    columns = df.word.unique().tolist()
+    index = df.state.unique().tolist()
+    new_df = pd.DataFrame(columns=columns, index=index)
+    print(f'time elapsed {time.time()-start} seconds')
     return new_df
 
 # obtaining emission probabilities from training data
-def df_calculate(df, new_df):
+def emissionMatrix(df, emission_matrix):
     '''
-    returns a matrix with the emission parameters matrix.
-    DF: the dataframe that contains the individual word instance and its label
-    NEW_DF: The matrix that has been generated from create_matrix() that will be used to place
+    emissionMatrix returns a matrix with the emission parameters matrix.
+    df: the dataframe that contains the individual word instance and its label
+    emission_matrix: The matrix that has been generated from createMatrix() that will be used to place
         the calculations
     '''
+    # start = time.time()
+    df_denominator = df.groupby('state').count()   # getting counts of states
+    df_counts = df.groupby(['state','word']).size().reset_index()   # getting counts of every word in each state
+    df_merged = df_counts.merge(df_denominator, left_on=['state'], right_on='state')  # merge 
+    df_merged = df_merged.rename(columns={"word_x": "word",0:"word_count", "word_y": "state_count"})
+    df_merged['Probability'] = df_merged.word_count/df_merged.state_count    # get emission probability (count of word in that state/ state count)
+    for index, row in tqdm(df_merged.iterrows()):  # for every known probabilty
+        emission_matrix.loc[row['state'],row['word']] = row['Probability']   # append into the emission matrix
+    emission_matrix = emission_matrix.fillna(0)   # fill those null cells with zero
+    # print(f'time elapsed {time.time()-start}')
+    return emission_matrix
 
-    unique_words = df['word'].unique().tolist()
-    count_all_states = df.groupby('state').count()
-    count = 0
-    for w in tqdm(unique_words):
-        word_df = [df.loc[df.word == w]][0]
-        # count for each state for each word
-        state_count = word_df.groupby('state').count()
-        for x in state_count.index:
-            numerator = (state_count['word'].loc[x])
-            denominator = (count_all_states['word'].loc[x])
-            new_df[x][count] = (numerator/denominator)
-        count += 1
-    new_df = new_df.fillna(0)
-    new_df = new_df.set_index(['unique_words'])
-    return new_df
-
-
-def df_calculate_2(df, new_df):
+def emissionMatrix_special(df, emission_matrix):
     '''
-    df_calculate while taking into account the special token
+    emissionMatrix_speical taking into account the special token with k=0.5
     Returns a matrix with the emission parameters matrix.
-    DF: the dataframe that contains the individual word instance and its label
-    NEW_DF: The matrix that has been generated from create_matrix() that will be used to place
+    df: the dataframe that contains the individual word instance and its label
+    emission_matrix: The matrix that has been generated from create_matrix() that will be used to place
         the calculations
     '''
-    unique_words = new_df['unique_words'].tolist()
-    count_all_states = df.groupby('state').count()
-    count = 0
-    for w in tqdm(unique_words):
-        word_df = [df.loc[df.word == w]][0]
-        # count for each state for each word
-        state_count = word_df.groupby('state').count()
-        for x in state_count.index:
-            numerator = (state_count['word'].loc[x])
-            denominator = (count_all_states['word'].loc[x])+0.5
-            new_df[x][count] = (numerator/denominator)
-        count += 1
-    unk_dict = {'unique_words': '#UNK#'}
-    for x in range(len(new_df.columns)-1):
-        unk_dict[new_df.columns[x]] = 0.5 / \
-            ((count_all_states['word'].loc[new_df.columns[x]])+0.5)
-    new_df = new_df.append(unk_dict, ignore_index=True)
-    new_df = new_df.fillna(0)
-    new_df = new_df.set_index(['unique_words'])
-    return new_df
+    k=0.5
+    df_denominator = df.groupby('state').count()   # getting counts of states
+    df_counts = df.groupby(['state','word']).size().reset_index()   # getting counts of every word in each state
+    df_merged = df_counts.merge(df_denominator, left_on=['state'], right_on='state')  # merge 
+    df_merged = df_merged.rename(columns={"word_x": "word",0:"word_count", "word_y": "state_count"})
+    df_merged['Probability'] = df_merged.word_count/(df_merged.state_count+k)    # get emission probability (count of word in that state/ state count)
+    for index, row in tqdm(df_merged.iterrows()):  # for every known probabilty
+        emission_matrix.loc[row['state'],row['word']] = row['Probability']   # append into the emission matrix
+    for i in df_train.state.unique().tolist():
+        emission_matrix.loc[i,'#UNK#'] = float(k/df_denominator.loc[i]+k)
+    emission_matrix = emission_matrix.fillna(0)   # fill those null cells with zero
+    return emission_matrix
 
-def argmax(new_df):
-    tag_dict={}
-    for index, row in new_df.iterrows():
-        tag_dict[index]=new_df.columns[row.argmax()]
-    return tag_dict
+# getting the most probable states given the words from the emission matrix
+def argmax(df):
+    start = time.time()
+    tags={}
+    for col in df.columns:
+        tags[col]=df.index[df[col].argmax()]
+    return tags
 
-def tag_system(tag_dict, testing_file):
-    test_df = load_test(testing_file)
-    test_ls = test_df['word'].tolist()
-
+# tagging test df with most probable states
+def tag_system(tags, df):
+    test_ls = df['word'].tolist()
     tag_states=[]
     for i in test_ls:
-        if i in tag_dict.keys():
-            tag_states.append(tag_dict[i])
+        if i in tags.keys():
+            tag_states.append(tags[i])
         elif i=="":   # for blank lines, set state to be blank
             tag_states.append("")
-        elif i not in tag_dict.keys():
-            tag_states.append(tag_dict['#UNK#'])
+        elif i not in tags.keys():
+            tag_states.append(tags['#UNK#'])
+    df['states']=tag_states
+    return df
+    
+# saving output as text file
+def save_df(df,path):
+    np.savetxt(path, df.values, fmt='%s')
+    # np.savetxt(r"EN\dev2_check.prediction", output.values, fmt='%s')
 
-    test_df['states']=tag_states
-    return test_df
+
+# def create_matrix(df):
+#     new_df = pd.DataFrame(columns=df['state'].unique().tolist())
+#     unique_words = df['word'].unique().tolist()
+#     new_df['unique_words'] = unique_words
+#     # Commented out for now
+#     # new_df = new_df.set_index('unique_words')
+#     return new_df
+
+# def df_calculate(df, new_df):
+#     '''
+#     returns a matrix with the emission parameters matrix.
+#     DF: the dataframe that contains the individual word instance and its label
+#     NEW_DF: The matrix that has been generated from create_matrix() that will be used to place
+#         the calculations
+#     '''
+
+#     unique_words = df['word'].unique().tolist()
+#     count_all_states = df.groupby('state').count()
+#     count = 0
+#     for w in tqdm(unique_words):
+#         word_df = [df.loc[df.word == w]][0]
+#         # count for each state for each word
+#         state_count = word_df.groupby('state').count()
+#         for x in state_count.index:
+#             numerator = (state_count['word'].loc[x])
+#             denominator = (count_all_states['word'].loc[x])
+#             new_df[x][count] = (numerator/denominator)
+#         count += 1
+#     new_df = new_df.fillna(0)
+#     new_df = new_df.set_index(['unique_words'])
+#     return new_df
+
+# def df_calculate_2(df, new_df):
+#     '''
+#     df_calculate while taking into account the special token
+#     Returns a matrix with the emission parameters matrix.
+#     DF: the dataframe that contains the individual word instance and its label
+#     NEW_DF: The matrix that has been generated from create_matrix() that will be used to place
+#         the calculations
+#     '''
+#     unique_words = new_df['unique_words'].tolist()
+#     count_all_states = df.groupby('state').count()
+#     count = 0
+#     for w in tqdm(unique_words):
+#         word_df = [df.loc[df.word == w]][0]
+#         # count for each state for each word
+#         state_count = word_df.groupby('state').count()
+#         for x in state_count.index:
+#             numerator = (state_count['word'].loc[x])
+#             denominator = (count_all_states['word'].loc[x])+0.5
+#             new_df[x][count] = (numerator/denominator)
+#         count += 1
+#     unk_dict = {'unique_words': '#UNK#'}
+#     for x in range(len(new_df.columns)-1):
+#         unk_dict[new_df.columns[x]] = 0.5 / \
+#             ((count_all_states['word'].loc[new_df.columns[x]])+0.5)
+#     new_df = new_df.append(unk_dict, ignore_index=True)
+#     new_df = new_df.fillna(0)
+#     new_df = new_df.set_index(['unique_words'])
+#     return new_df
+
+# def argmax(new_df):
+#     tags={}
+#     for index, row in new_df.iterrows():
+#         tags[index]=new_df.columns[row.argmax()]
+#     return tags
+
+# def tag_system(tags, testing_file):
+#     df = load_test(testing_file)
+#     test_ls = df['word'].tolist()
+
+#     tag_states=[]
+#     for i in test_ls:
+#         if i in tags.keys():
+#             tag_states.append(tags[i])
+#         elif i=="":   # for blank lines, set state to be blank
+#             tag_states.append("")
+#         elif i not in tags.keys():
+#             tag_states.append(tags['#UNK#'])
+
+#     df['states']=tag_states
+#     return df
 
 
 
