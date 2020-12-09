2803525c9eae82d7c8ae8b6a773c84e7827bc1d5
jeroee <jeroejeroe@gmail.com>
2020-12-09 22:29:44 +0800

    cleaned part 3

--- Part_3.py
+++ Part_3.py
@@ -4,18 +4,11 @@ from tqdm import tqdm
 from collections import Counter
 from Part_2 import *
 
-# EN_train = 'EN/train'
-# SG_train = 'SG/train'
-# CN_train = 'CN/train'
-# EN_test = 'EN/dev.in'
-# SG_test = 'SG/dev.in'
-# CN_test = 'CN/dev.in'
-
-# load training file
+# load train file for transition matrix
 
 
 def load_train_trans(training_file):
-    f = open(training_file)
+    f = open(training_file, encoding="utf8")
     ls_state = ['START']
     for line in f:
         item = line.strip('\n').split(' ')
@@ -27,12 +20,13 @@ def load_train_trans(training_file):
     ls_state.pop(-1)
     return ls_state
 
-# load test files
+
+# load test file dev.in into a single column df
 
 
 def load_test(testing_file):
     ls = []
-    f = open(testing_file)
+    f = open(testing_file, encoding="utf8")
     for line in f:
         ls.append(line.strip('\n'))
     df = pd.DataFrame(ls)
@@ -54,13 +48,11 @@ def transition_matrix(training_list):
     return rls_matrix
 
 
-# Implement the Function
-# temp = load_train_trans(EN_train)
-# tran_matrix = transition_matrix(temp)
-# print(tran_matrix)
-
 # Part 2 Vertibri Algo
-def load_vertibri_test(file):
+# load test file into a nested list
+
+
+def pre_vertibri_load(file):
     m = open(file, encoding="utf8")
     ls = []
     big_ls = []
@@ -74,104 +66,12 @@ def load_vertibri_test(file):
     return big_ls
 
 
-'''
-Full Vertibri Algorithm
-'''
-
-
 def log(x, inf_replace=-1000):
     x = x.replace(0, np.nan)
     out = np.log(x)
     out = out.replace(np.nan, -1000)
     # out[~np.isfinite(out)] = inf_replace
     return out
-
-
-def Vertibri(documents, emission_matrix, transition_matrix):
-    # forward propogation
-    logged_emission = log(emission_matrix)
-    logged_transition = log(transition_matrix)
-    transition_np = logged_transition.drop(
-        ['START']).drop('STOP', axis=1).to_numpy()
-    states = emission_matrix.index.tolist()
-    big_ls = []
-    # Vertibri = []
-    print('forward propogation')
-    for document in tqdm(documents):
-        Vertibri = []
-        forward_steps = len(document)+1
-        for i in range(forward_steps):
-            if i == 0:  # for from START to first layer
-                if document[i] in tags.keys():
-                    layer = [
-                        t+e for t, e in zip(logged_transition.loc['START'].drop('STOP'), logged_emission[document[i]])]
-                elif document[i] not in tags.keys():
-                    layer = [
-                        t+e for t, e in zip(logged_transition.loc['START'].drop('STOP'), logged_emission['#UNK#'])]
-                Vertibri.append(layer)
-            elif i != 0 and i != forward_steps-1:  # not first or last step
-                prev_layer_prob = Vertibri[-1]*21
-                prev_layer_prob = np.array(prev_layer_prob).reshape(21, 21).T
-                m = prev_layer_prob + transition_np
-                if document[i] in tags.keys():
-                    emission_ls = logged_emission[document[i]].tolist()*21
-                    emission_np = np.array(emission_ls).reshape(21, 21)
-                elif document[i] not in tags.keys():
-                    emission_ls = logged_emission['#UNK#'].tolist()*21
-                    emission_np = np.array(emission_ls).reshape(21, 21)
-                matrix = (m + emission_np)
-                layer = np.amax(matrix, 0)
-                Vertibri.append(layer.tolist())
-            elif i == forward_steps-1:
-                prev_layer_prob = np.array(Vertibri[-1])
-                last = logged_transition.drop('START')['STOP'].tolist()
-                layer = prev_layer_prob+last
-                # gets the position of the maximum in the last one
-                position_last_max = (layer.tolist().index(max(layer)))
-                Vertibri.append(layer.tolist())
-        big_ls.append(Vertibri)
-    # -------------------------------------------------------------------------------
-    # backward propogation
-
-    # Stores the states
-    big_state = []
-    state_order = []
-    print('backward propogation')
-    for Vertibri in tqdm(big_ls):
-        # Create a trimmed one with all less the stop column
-        Vertibri_trim = Vertibri[:-1]
-
-        # Iterate through each layer find the argmax for the layer and continue
-        for layer in Vertibri_trim[::-1]:
-            # List that holds the summed value of the last term with the second last layer
-            intermediate_ls = []
-            intermediate_ls = [
-                x+transition_np[layer.index(x), pos_max_layer] for x in layer]
-            # Find the argmax for the layer
-            pos_max_value = np.max(intermediate_ls)
-            pos_max_layer = np.argmax(intermediate_ls)
-            # Insert into the state order
-            state_order.insert(0, states[pos_max_layer])
-            # Update the value of position_last_max
-            position_last_max = pos_max_value
-        big_state.append(state_order)
-
-
-    # -------------------------------------------------------------------------------
-'''
-END
-'''
-# tagging words
-
-
-def tag_system_3(states, df):
-    states_ls = []
-    for state in states:
-        for i in state:
-            states_ls.append(i)
-        states_ls.append(' ')
-    df['states'] = states_ls
-    return df
 
 
 def getHelpers(emission_matrix, transition_matrix):
@@ -183,8 +83,8 @@ def getHelpers(emission_matrix, transition_matrix):
     return logged_emission, logged_transition, transition_np, states
 
 
-def Vertibri2(tweet, logged_emission, logged_transition, transition_np, states, tags):
-    Vertibri = []
+def single_Viterbi(tweet, logged_emission, logged_transition, transition_np, states, tags):
+    Viterbi = []
     forward_steps = len(tweet)+1
     for i in range(forward_steps):
         if i == 0:  # for from START to first layer
@@ -194,9 +94,9 @@ def Vertibri2(tweet, logged_emission, logged_transition, transition_np, states, 
             elif tweet[i] not in tags.keys():
                 layer = [
                     t+e for t, e in zip(logged_transition.loc['START'].drop('STOP'), logged_emission['#UNK#'])]
-            Vertibri.append(layer)  # append first layer
+            Viterbi.append(layer)  # append first layer
         elif i != 0 and i != forward_steps-1:  # not first or last layer
-            prev_layer_prob = Vertibri[-1]*21
+            prev_layer_prob = Viterbi[-1]*21
             prev_layer_prob = np.array(prev_layer_prob).reshape(21, 21).T
             m = prev_layer_prob + transition_np
             if tweet[i] in tags.keys():
@@ -207,20 +107,20 @@ def Vertibri2(tweet, logged_emission, logged_transition, transition_np, states, 
                 emission_np = np.array(emission_ls).reshape(21, 21)
             matrix = (m + emission_np)
             layer = np.amax(matrix, 0)
-            Vertibri.append(layer.tolist())
+            Viterbi.append(layer.tolist())
         elif i == forward_steps-1:
-            prev_layer_prob = np.array(Vertibri[-1])
+            prev_layer_prob = np.array(Viterbi[-1])
             last = logged_transition.drop('START')['STOP'].tolist()
             layer = prev_layer_prob+last
             # gets the position of the maximum in the last one
             pos_max_layer = (layer.tolist().index(max(layer)))
-            Vertibri.append(layer.tolist())
+            Viterbi.append(layer.tolist())
             # this step is correct alr, forward prop done
     # back prop starts here
-    Vertibri_trim = Vertibri[:-1]
+    Viterbi_trim = Viterbi[:-1]
     state_order = []
     # Iterate through each layer find the argmax for the layer and continue
-    for layer in Vertibri_trim[::-1]:
+    for layer in Viterbi_trim[::-1]:
         # List that holds the summed value of the last term with the second last layer
         intermediate_arr = np.array(layer)
         intermediate_ls = intermediate_arr + transition_np[:, pos_max_layer]
@@ -232,104 +132,33 @@ def Vertibri2(tweet, logged_emission, logged_transition, transition_np, states, 
     return state_order
 
 
-def Vertibri(documents, emission_matrix, transition_matrix):
-    # forward propogation
-    logged_emission = log(emission_matrix)
-    logged_transition = log(transition_matrix)
-    transition_np = logged_transition.drop(
-        ['START']).drop('STOP', axis=1).to_numpy()
-    states = emission_matrix.index.tolist()
-    big_ls = []
-    # Vertibri = []
-    print('forward propogation')
-    for document in tqdm(documents):
-        Vertibri = []
-        forward_steps = len(document)+1
-        for i in range(forward_steps):
-            if i == 0:  # for from START to first layer
-                if document[i] in tags.keys():
-                    layer = [
-                        t+e for t, e in zip(logged_transition.loc['START'].drop('STOP'), logged_emission[document[i]])]
-                elif document[i] not in tags.keys():
-                    layer = [
-                        t+e for t, e in zip(logged_transition.loc['START'].drop('STOP'), logged_emission['#UNK#'])]
-                Vertibri.append(layer)
-            elif i != 0 and i != forward_steps-1:  # not first or last step
-                prev_layer_prob = Vertibri[-1]*21
-                prev_layer_prob = np.array(prev_layer_prob).reshape(21, 21).T
-                m = prev_layer_prob + transition_np
-                if document[i] in tags.keys():
-                    emission_ls = logged_emission[document[i]].tolist()*21
-                    emission_np = np.array(emission_ls).reshape(21, 21)
-                elif document[i] not in tags.keys():
-                    emission_ls = logged_emission['#UNK#'].tolist()*21
-                    emission_np = np.array(emission_ls).reshape(21, 21)
-                matrix = (m + emission_np)
-                layer = np.amax(matrix, 0)
-                Vertibri.append(layer.tolist())
-            elif i == forward_steps-1:
-                prev_layer_prob = np.array(Vertibri[-1])
-                last = logged_transition.drop('START')['STOP'].tolist()
-                layer = prev_layer_prob+last
-                # gets the position of the maximum in the last one
-                position_last_max = (layer.tolist().index(max(layer)))
-                Vertibri.append(layer.tolist())
-        big_ls.append(Vertibri)
-    # -------------------------------------------------------------------------------
-    # backward propogation
-
-    # Stores the states
-    big_state = []
-    state_order = []
-    print('backward propogation')
-    for Vertibri in tqdm(big_ls):
-        # Create a trimmed one with all less the stop column
-        Vertibri_trim = Vertibri[:-1]
-
-        # Iterate through each layer find the argmax for the layer and continue
-        for layer in Vertibri_trim[::-1]:
-            # List that holds the summed value of the last term with the second last layer
-            intermediate_ls = []
-            intermediate_ls = [
-                x+transition_np[layer.index(x), pos_max_layer] for x in layer]
-            # Find the argmax for the layer
-            pos_max_value = np.max(intermediate_ls)
-            pos_max_layer = np.argmax(intermediate_ls)
-            # Insert into the state order
-            state_order.insert(0, states[pos_max_layer])
-            # Update the value of position_last_max
-            position_last_max = pos_max_value
-        big_state.append(state_order)
-
-
-file_train = 'EN/train'
-file_test = 'EN/dev.in'
-df_train = load_train(file_train)
-emission_matrix = createMatrix(df_train)
-emission_matrix = emissionMatrix_special(df_train, emission_matrix)
-tags = argmax(emission_matrix)
-print('getting transition matrix')
-ls = load_train_trans(file_train)
-transition_matrix = transition_matrix(ls)
-logged_emission, logged_transition, transition_np, states = getHelpers(
-    emission_matrix, transition_matrix)
-tweets = load_vertibri_test(file_test)
-#------------------ big vertibri ---------------------
-big_states = []
-for tweet in tqdm(tweets):
-    single_state = Vertibri2(tweet, logged_emission, logged_transition,
-                             transition_np, states, tags)
-    single_state.append(' ')
-    big_states.append(single_state)
-# flat map this shit
-states = [item for single_state in big_states for item in single_state]
-#---------------------
-df = load_test(file_test)
-df['states']=states
-path = 'EN/dev_p3.pred'
-save_df(df,path)
-
-
+# this is cfm alr
+def output(file_train, file_test, path):
+    # file_train = 'EN/train'
+    # file_test = 'EN/dev.in'
+    df_train = load_train(file_train)
+    emission_matrix = createMatrix(df_train)
+    emission_matrix = emissionMatrix_special(df_train, emission_matrix)
+    tags = argmax(emission_matrix)
+    print('getting transition matrix')
+    ls = load_train_trans(file_train)
+    transition_matrix1 = transition_matrix(ls)
+    logged_emission, logged_transition, transition_np, states = getHelpers(
+        emission_matrix, transition_matrix1)
+    tweets = pre_vertibri_load(file_test)
+    # ------------------ big vertibri ---------------------
+    big_states = []
+    for tweet in tqdm(tweets):
+        single_state = single_Viterbi(tweet, logged_emission, logged_transition,
+                                      transition_np, states, tags)
+        single_state.append(' ')
+        big_states.append(single_state)
+    # flat map this shit
+    states = [item for single_state in big_states for item in single_state]
+    # ---------------------
+    df = load_test(file_test)
+    df['states'] = states
+    save_df(df, path)
 
 
 # print(test)
@@ -338,18 +167,27 @@ save_df(df,path)
 
 
 # # file paths
-# EN_train = 'EN/train'
-# SG_train = 'SG/train'
-# CN_train = 'CN/train'
-# EN_test = 'EN/dev.in'
-# SG_test = 'SG/dev.in'
-# CN_test = 'CN/dev.in'
-# EN_pred_3 = 'EN/dev_p3.pred'
-# SG_pred_3 = 'SG/dev_p3.pred'
-# CN_pred_3 = 'CN/dev_p3.pred'
+EN_train = 'EN/train'
+SG_train = 'SG/train'
+CN_train = 'CN/train'
+EN_test = 'EN/dev.in'
+SG_test = 'SG/dev.in'
+CN_test = 'CN/dev.in'
+EN_pred_3 = 'EN/dev_p3.pred'
+SG_pred_3 = 'SG/dev_p3.pred'
+CN_pred_3 = 'CN/dev_p3.pred'
 # files = [['EN', EN_train, EN_test, EN_pred_3],
 #          ['SG', SG_train, SG_test, SG_pred_3],
 #          ['CN', CN_train, CN_test, CN_pred_3]]
+
+print('Starting Part 3')
+start_time = time.time()
+output(EN_train, EN_test, EN_pred_3)
+output(SG_train, SG_test, SG_pred_3)
+output(CN_train, CN_test, CN_pred_3)
+print('Part 3 Complete')
+print(f'time elapsed {time.time()-start_time} seconds')
+
 
 # print('Starting Part 3')
 # start_time = time.time()
